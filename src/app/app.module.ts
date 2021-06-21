94830a8ea810b6e8314a7f60624c5a1f34f81ecb
KBENBENECK <kbenbeneck@gmail.com>
2021-06-21 19:59:25 -0400

    Create post-create comp. .ts and .html and add to app declarations arr. Render text area input and button.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
     BrowserModule
