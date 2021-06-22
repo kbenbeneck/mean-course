fa5b1eb542accc2849f102edba366e9238057660
KBENBENECK <kbenbeneck@gmail.com>
2021-06-22 16:22:19 -0400

    Fetch dummy posts from /api/posts path.

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,30 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
 
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: '1',
+      title: "title 1",
+      content: "content 1"
+    },
+    {
+      id: '2',
+      title: "title 2",
+      content: "content 2"
+    },
+    {
+      id: '3',
+      title: "title 3",
+      content: "content 3"
+    },
+  ]
+  res.status(200).json({
+    message: "Posts fetched succesfully!",
+    posts: posts
+  });
 });
 
 module.exports = app;
