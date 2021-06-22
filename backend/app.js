2b9af1cd966e9145d4c61fdd6408b51471afdeda
KBENBENECK <kbenbeneck@gmail.com>
2021-06-22 16:45:48 -0400

    Configure CORS, allow headers, methods.

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,16 @@ const express = require('express');
 
 const app = express();
 
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers",
+  "Origin, X-Requested_With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Methods",
+  "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
 
 app.use('/api/posts', (req, res, next) => {
   const posts = [
