6613dc9b1c07a34bbb34a708e4cde9ebb9c9c0f5
KBENBENECK <kbenbeneck@gmail.com>
2021-06-26 15:11:24 -0400

    Add posts with id.

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -36,9 +36,11 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = { id: '', title: title, content: content};
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+    this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
       .subscribe((responseData) => {
         console.log(responseData.message)
+        const id = responseData.postId;
+        post.id = id
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
       });
@@ -48,6 +50,9 @@ export class PostsService {
     this.http.delete("http://localhost:3000/api/posts/" + postId)
       .subscribe(() => {
         console.log("Deleted!")
+        const updatedPosts = this.posts.filter(post => post.id !== postId);
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts])
       })
   }
 }
