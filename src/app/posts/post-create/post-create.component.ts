f1d48326dcc738f355247027bd3198e99178f972
KBENBENECK <kbenbeneck@gmail.com>
2021-06-28 12:36:27 -0400

    Add image preview

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -16,6 +16,7 @@ export class PostCreateComponent implements OnInit {
   post: Post | any;
   isLoading = false
   form!: FormGroup;
+  imagePreview: string
   private mode = 'create';
   private postId: string | any;
 
@@ -54,8 +55,11 @@ export class PostCreateComponent implements OnInit {
     const file = (event.target as HTMLInputElement).files[0];
     this.form.patchValue({ 'image': file });
     this.form.get('image').updateValueAndValidity();
-    console.log(file)
-    console.log(this.form)
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string
+    };
+    reader.readAsDataURL(file);
   }
   onSavePost() {
     if (this.form.invalid) { return; }
