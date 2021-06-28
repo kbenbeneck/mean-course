846365afb13023e95972a755a5fbf87ce3678730
KBENBENECK <kbenbeneck@gmail.com>
2021-06-28 12:02:23 -0400

    Add image controls to store the image. Remove strict mode.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -28,6 +28,9 @@ export class PostCreateComponent implements OnInit {
       }),
       'content': new FormControl(null, {
         validators: [Validators.required]
+      }),
+      'image': new FormControl(null, {
+        validators: [Validators.required]
       })
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -45,6 +48,14 @@ export class PostCreateComponent implements OnInit {
         this.postId = null;
       }
     });
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ 'image': file });
+    this.form.get('image').updateValueAndValidity();
+    console.log(file)
+    console.log(this.form)
   }
   onSavePost() {
     if (this.form.invalid) { return; }
