c42728aae729183e4cc15489ffcb9ee031198e72
KBENBENECK <kbenbeneck@gmail.com>
2021-07-05 14:27:34 -0400

    Add signup logic to backend. Bcrypt pw. Add auth service class and connect to backend.

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,14 +1,21 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
+import { AuthService } from "../auth.service";
+
 @Component({
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.css"]
 })
 export class SignupComponent {
   isLoading = false;
 
+  constructor(public authService: AuthService) {}
+
   onSignup(form: NgForm) {
-    console.log(form.value)
+    if (form.invalid) {
+      return;
+    }
+    this.authService.createUser(form.value.email, form.value.password);
   }
 }
