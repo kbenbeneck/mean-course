fac2e5c49ca5996ed14eeb37776a0a74612f428a
KBENBENECK <kbenbeneck@gmail.com>
2021-07-06 12:52:14 -0400

    Add middleware to protect routes.

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 const Post = require('../models/post');
+const checkAuth = require("../middleware/check-auth");
 const router = express.Router();
 
 const MIME_TYPE_MAP = {
@@ -23,7 +24,11 @@ const storage = multer.diskStorage({
     cb(null, name + '-' + Date.now() + '.' + ext);
   }
 });
-router.post('', multer({storage: storage}).single("image"), (req, res, next) => {
+router.post(
+  '',
+  checkAuth,
+  multer({storage: storage}).single("image"),
+  (req, res, next) => {
   const url = req.protocol + '://' + req.get("host")
 
   const post = new Post({
@@ -44,6 +49,7 @@ router.post('', multer({storage: storage}).single("image"), (req, res, next) => 
 });
 router.patch(
   "/:id",
+  checkAuth,
   multer({ storage: storage }).single("image"),
   (req, res, next) => {
     let imagePath = req.body.imagePath;
@@ -97,7 +103,10 @@ router.get("/:id", (req, res, next) => {
     }
   });
 });
-router.delete("/:id", (req, res, next) => {
+router.delete(
+  "/:id",
+  checkAuth,
+  (req, res, next) => {
   Post.deleteOne({ _id: req.params.id }).then(result => {
     console.log(result);
     res.status(200).json({ message: "Post deleted!" });
