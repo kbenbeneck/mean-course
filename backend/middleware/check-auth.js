28eba07b6d22802b5902dd1f6baacf12e79b168c
KBENBENECK <kbenbeneck@gmail.com>
2021-07-07 18:16:10 -0400

    Add user id to posts.

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -3,8 +3,12 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, 'secret_this_should_be_longer');
+    const decodedToken = jwt.verify(token, 'secret_this_should_be_longer');
     next();
+    req.userData = {
+      email: decodedToken.email,
+      userId: decodedToken.userId
+    };
   } catch (error) {
     res.status(401).json({message: "Auth failed"});
   }
